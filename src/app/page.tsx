ee8605a2a70b42dc11fb00e6afabbe73d3962e95
JRivers-pt <joaodmribeiro@outlook.com>
2026-02-18 16:16:36 +0000

    chore: remove debug version tracer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,6 @@ import {
   HelpCircle
 } from "lucide-react"
 
-// TRACER: Use this to verify if the new code is actually deployed
-const APP_VERSION = "DEPLOY-CHECK-003"
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
